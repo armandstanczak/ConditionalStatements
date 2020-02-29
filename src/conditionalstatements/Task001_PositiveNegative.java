eed6a023ab8d1ebde07975c8cc91d14219202e37
Armand Stanczak <armandstanczak@gmail.com>
2020-02-29 13:33:20 +0100

    Conditional Statement tasks

---
+++ src/conditionalstatements/Task001_PositiveNegative.java
@@ -0,0 +1,16 @@
+package conditionalstatements;
+
+/**
+ *
+ * @author Armand Stańczak
+ */
+public class Task001_PositiveNegative {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
