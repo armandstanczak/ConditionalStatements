5255deb6a8478b24c7609b18cd10c9530d75b088
Armand Stanczak <armandstanczak@gmail.com>
2020-03-01 19:07:02 +0100

    Conditional Statement tasks

--- src/conditionalstatements/Task025_PatternTriangle.java
+++ src/conditionalstatements/Task025_PatternTriangle.java
@@ -10,14 +10,18 @@ public class Task025_PatternTriangle {
         System.out.print("Input number of rows: ");
         int rows = input.nextInt();
         int counter = 1;
+        int counter2 = rows * 2 - 3;
         int spaces = rows - 1;
+        int additionalRows = rows - 1;
+        int countEmpy = 1;
+        int temp;
         
         for (int i = 1; i <= rows; i++) {
             for (int j = 1; j <= spaces; j++) {
                 System.out.print(" ");
             }
             spaces--;
-            int temp = i;
+            temp = i;
             for (int k = 0; k < counter; k++) { 
                 if (k < counter / 2) {
                     System.out.print(temp--);
@@ -25,7 +29,24 @@ public class Task025_PatternTriangle {
                     System.out.print(temp++);
                 }
             }
-            counter = (i + 1) * 2 - 1;
+            counter = counter + 2;
+            System.out.println("");
+        }
+        
+        for (int x = 1; x <= additionalRows; x++) {
+            for (int y = 1; y <= countEmpy; y++) {
+                System.out.print(" ");
+            }
+            countEmpy++;
+            temp = rows - x;
+            for (int z = 0; z < counter2; z++) { 
+                if (z < counter2 / 2) {
+                    System.out.print(temp--);
+                } else {
+                    System.out.print(temp++);
+                }
+            }
+            counter2 = counter2 - 2;
             System.out.println("");
         }
     }
